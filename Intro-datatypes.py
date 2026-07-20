0dac5199f87c639a9cd4f59494de1f03d136b89f
rohanaamir-pk <rohan.aamir.pk@gmail.com>
2026-07-20 09:21:32 +0500

    labeled types

--- Intro-datatypes.py
+++ Intro-datatypes.py
@@ -1,14 +1,14 @@
 #intro to python----//DAY-1//
 #DATA TYPEs
 
-a = 10
-b= 10.432
-c = 't'
-d = "Hello"
-e = False
-f = [12, 32, 45]
-g = {23, 45, 67}
-h = (111, 212, 3)
+a = 10  #integer/whole numbers
+b= 10.432 #float/decimal point
+c = 't'   #string
+d = "Hello"     #string
+e = False       #boolean
+f = [12, 32, 45]        #list
+g = {23, 45, 67}        #tuple
+h = (111, 212, 3)       #set
 
 print(a, type(a))
 print(b, type(b))
